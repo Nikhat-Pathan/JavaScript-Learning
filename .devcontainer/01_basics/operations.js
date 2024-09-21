2c1e96ac15ea06eabbb84b2ee83c9ee562ab9bac
Nikhat-Pathan <prashid460@gmail.com>
2024-09-21 04:08:07 +0000

    operations

---
+++ .devcontainer/01_basics/operations.js
@@ -0,0 +1,42 @@
+//***************************** OPERATIONS ********************************* */
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);        //2 to the power 3
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Nikhat"
+console.log(str1+str2);
+
+console.log("1"+2);   //12
+console.log(1+"2");    //12
+console.log("1"+2+2);  //122
+console.log(1+2+"2");  //32
+
+console.log(+true);    //1
+//console.log(true+);    => Error (not allowed)
+console.log(+"");      //0
+let num1,num2,num3;
+num1=num2=num3= 2+2
+console.table([num1,num2,num3]);
+
+//Link ro studey
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
+
+
+
+
+
+
+
